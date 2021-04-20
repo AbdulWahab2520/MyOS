8a96e3dddfe57bfb47198c75e9575e687eac0626
Abdul Wahab <57444590+AbdulWahab2520@users.noreply.github.com>
2021-04-21 00:32:17 +0500

    Add files via upload

---
+++ MyOS/src/impl/kernel/main.c
@@ -0,0 +1,149 @@
+#include "print.h"
+
+// Number of lines for the alphabet's pattern
+int height = 5;
+// Number of character width in each line
+int width = 9;
+void kernel_main(){
+   print_clear();
+
+
+ print_str("\n\n\n");
+
+print_str("       ABDUL WAHAB       \n");
+print_str("|________________________|\n");
+print_str("|----BSCS 9B (285882)----|\n");
+
+print_str("\n\n\n\n\n");
+
+	char ch1 = 'N';
+		print_Pattern(ch1);
+
+    char ch2 = 'U';
+        print_Pattern(ch2);
+        print_str("\n");
+
+    char ch3 = 'S';
+        print_Pattern(ch3);
+        print_str("\n");
+
+    char ch4 = 'T';
+		print_Pattern(ch4);
+
+}
+
+
+    
+int abs(int d){
+    return d < 0 ? -1 * d : d;
+}
+
+
+//To print " N " on the console 
+void print_N()
+{    print_set_color(PRINT_COLOR_WHITE,PRINT_COLOR_BLACK);
+    int i, j, counter = 0;
+    for (i = 0; i < height; i++) {
+        print_str("*");
+        for (j = 0; j <= height; j++) {
+            if (j == height)
+                print_str("*");
+            else if (j == counter)
+                print_str("*");
+            else
+                print_str(" ");
+        }
+        counter++;
+        print_str("\n");
+    }
+}
+
+
+  
+//To print " U " on the console
+void print_U()
+{ print_set_color(PRINT_COLOR_WHITE,PRINT_COLOR_BLACK);
+    int i, j;
+    for (i = 0; i < height; i++) {
+        if (i != 0 && i != height - 1)
+            print_str("*");
+        else
+            print_str(" ");
+        for (j = 0; j < height; j++) {
+            if (((i == height - 1)
+                 && j >= 0
+                 && j < height-1 ))
+                print_str("*");
+            else if (j == height - 1 && i != 0
+                     && i != height - 1)
+                print_str("*");
+            else
+                print_str(" ");
+        }
+        print_str("\n");
+    }
+}
+
+
+//To print " S " on the console
+void print_S()
+{  print_set_color(PRINT_COLOR_WHITE,PRINT_COLOR_BLACK);
+    int i, j;
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < height; j++) {
+            if ((i == 0 || i == height / 2
+                 || i == height - 1))
+                print_str("*");
+            else if (i < height / 2
+                     && j == 0)
+                print_str("*");
+            else if (i > height / 2
+                     && j == height - 1)
+                print_str("*");
+            else
+                print_str(" ");
+        }
+        print_str("\n");
+    }
+}
+  
+//To print " T " on the console
+void print_T()
+{ print_set_color(PRINT_COLOR_WHITE,PRINT_COLOR_BLACK);
+    int i, j;
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < height; j++) {
+            if (i == 0)
+                print_str("*");
+            else if (j == height / 2)
+                print_str("*");
+            else
+                print_str(" ");
+        }
+        print_str("\n");
+    }
+}
+
+
+void print_Pattern(char character)
+{
+    switch (character) {
+    
+    case 'N':
+        print_N();
+        break;
+    case 'U':
+        print_U();
+        break;
+    case 'S':
+        print_S();
+        break;
+    case 'T':
+        print_T();
+        break;
+    default:
+        print_str("Wrong Input");
+    
+    
+    }
+}
